6adb7baad1156f658f7736b8922dc575474ae763
tommy3713 <tommy3713@gmail.com>
2023-02-06 22:45:01 -0800

    refactor: add player controller and fix camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-   [SerializeField] private Transform player;
-
+    private Transform player;
+    //should fix in future
+    private string PLAYER_NAME = "player(Clone)";
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
+
+        player = GameObject.Find(PLAYER_NAME).transform; 
         transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
     }
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SwitchPlayer();
+    }
+
+    private void SwitchPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Instantiate(player);
+            //this will add multiple scripts
+            player.AddComponent<BasicPlayer>();
+        }
+    }
+}
