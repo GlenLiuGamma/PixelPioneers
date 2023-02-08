20ced2a1faf07326aa23b0cd343330890d353e84
yuchewang <yc.wang1009@gmail.com>
2023-02-07 16:28:29 -0800

    refactor: complete player used time counting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,39 +7,61 @@ public class PlayerController : MonoBehaviour
 
     public GameObject player;
     private string[] playerList = { "BasicPlayer", "DashPlayer","AntiGravityPlayer" };
-
+    private bool isBasicPlayer = true;
+    private float timeLeft = 10f;
     // Start is called before the first frame update
     void Start()
     {
+        player.AddComponent<BasicPlayer>();
+        isBasicPlayer = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         SwitchPlayer();
+        UpdateTime();
+
     }
 
     private void SwitchPlayer()
     {
         if ( Input.GetKeyDown(KeyCode.Z))
         {
+            isBasicPlayer = true;
             Debug.Log("Pressed Z");
             DestroyAll();
             player.AddComponent<BasicPlayer>();
         }
-        if (Input.GetKeyDown(KeyCode.X))
+        if (timeLeft > 0 && Input.GetKeyDown(KeyCode.X))
         {
+            isBasicPlayer = false;
             Debug.Log("Pressed X");
             DestroyAll();
             player.AddComponent<DashPlayer>();
         }
-        if (Input.GetKeyDown(KeyCode.C))
+        if (timeLeft > 0 && Input.GetKeyDown(KeyCode.C))
         {
-            Debug.Log("Pressed X");
+            isBasicPlayer = false;
+            Debug.Log("Pressed C");
             DestroyAll();
             player.AddComponent<AntiGravityPlayer>();
         }
     }
+    private void UpdateTime(){
+        Debug.Log(timeLeft);
+        if (!isBasicPlayer){
+            timeLeft-= 1 * Time.deltaTime;
+        }
+        if(timeLeft <= 0){
+            timeLeft = 0;
+            isBasicPlayer = true;
+            DestroyAll();
+            player.AddComponent<BasicPlayer>();
+        }
+    }
+
     private void DestroyAll()
     {
         foreach(string playerType in playerList)
@@ -47,4 +69,5 @@ public class PlayerController : MonoBehaviour
             Destroy(player.GetComponent(playerType));
         }
     }
+    
 }
