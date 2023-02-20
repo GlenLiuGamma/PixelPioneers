890cc7316f84acee739bd3af71fc24274709c279
匡祖平 <steven8748@gmail.com>
2023-02-19 19:30:08 -0800

    Updating analytics script

--- Assets/Scripts/SendToGoogle.cs
+++ Assets/Scripts/SendToGoogle.cs
@@ -9,6 +9,8 @@ public class SendToGoogle : MonoBehaviour
     [SerializeField] private string URL;
     private long _sessionID;
     [SerializeField] private GameEvent _eventCount;
+    [SerializeField] private string Level;
+
     // Start is called before the first frame update
     public GameObject player_control;
     private void Awake()
@@ -23,7 +25,8 @@ public class SendToGoogle : MonoBehaviour
     }
 
 
-     private IEnumerator Post(string sessionID, string DeathCnt, string time, string TimeLeft, string DeathReason, string DeathPosition, string CurrentCharacter )
+        private IEnumerator Post(string sessionID, string DeathCnt, string time, string TimeLeft, string DeathReason, string DeathPosition, string CurrentCharacter, string Level )
+
     {
     // Create the form and enter responses
     Debug.Log("In the script");
@@ -36,6 +39,8 @@ public class SendToGoogle : MonoBehaviour
     death reason: entry.1646797438
     death position: entry.69297460
     death character: entry.1912151537
+    Level: entry.246051495
+
     */
     form.AddField("entry.1277992947", sessionID);
     form.AddField("entry.1776729504", TimeLeft);
@@ -44,6 +49,8 @@ public class SendToGoogle : MonoBehaviour
     form.AddField("entry.1646797438", DeathReason);
     form.AddField("entry.69297460", DeathPosition);
     form.AddField("entry.1912151537", CurrentCharacter);
+    form.AddField("entry.246051495", Level);
+
     // Send responses and verify result
     using (UnityWebRequest www = UnityWebRequest.Post(URL, form))
     {
@@ -65,7 +72,8 @@ public class SendToGoogle : MonoBehaviour
      string Timeleft = "";
      PlayerController time_left = player_control.GetComponent<PlayerController>();
      Timeleft = time_left.timeLeft.ToString();
-     StartCoroutine(Post(_sessionID.ToString(),  _eventCount.Deathcnt.ToString(), Time.time.ToString(), Timeleft, DeathReason, DeathPosition, CurrentCharacter ));
+     StartCoroutine(Post(_sessionID.ToString(),  _eventCount.Deathcnt.ToString(), Time.time.ToString(), Timeleft, DeathReason, DeathPosition, CurrentCharacter, Level));
+
      /*
      1. Amount of time to switch characters - _eventCount.Timeleft
      2. Time to pass the level - Time.time
