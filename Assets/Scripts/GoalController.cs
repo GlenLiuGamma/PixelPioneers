2d81660feb9957b347f491ea38c184a59ce8d864
匡祖平 <steven8748@gmail.com>
2023-04-03 12:09:22 -0700

    Fix level1 to level2

--- Assets/Scripts/GoalController.cs
+++ Assets/Scripts/GoalController.cs
@@ -23,8 +23,12 @@ public class GoalController : MonoBehaviour
         if (other.gameObject.CompareTag("Goal_Level1"))
         {
             
-            //pauseMenuUI.transform.Find("ResumeButton").transform.gameObject.SetActive(false);
-            //pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            pauseMenuUI.transform.Find("NextLevel").transform.gameObject.SetActive(true);
+            pauseMenuUI.transform.Find("ResumeButton").transform.gameObject.SetActive(false);
+            pauseMenuUI.SetActive(true);
+            Debug.Log("Pass the tutorial after pauseMenuUI");
+
             string DeathReason = "Goal_Level1";
             string GoalPosition = "Goal_Level1";
             string CurrentCharacter = "Goal_Level1";
@@ -32,7 +36,7 @@ public class GoalController : MonoBehaviour
             DataStorage.Deathcnt = 0;
             DataStorage.sessionID = 0;
             DataStorage.coin_count = 0;
-            SceneManager.LoadScene("GameOver");//Final stage will load the GameOver Scene
+
         } else if(other.gameObject.CompareTag("Goal_Tutorial1")){
             Time.timeScale = 0f;
             pauseMenuUI.transform.Find("NextLevel").transform.gameObject.SetActive(true);
