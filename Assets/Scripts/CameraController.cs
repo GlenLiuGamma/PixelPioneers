79ef83e9b00ef223a94fc46158fe474d24c6a387
GlenLiu <thes60415@gmail.com>
2023-04-09 00:24:52 -0700

    finish melon zooming

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,10 +18,37 @@ public class CameraController : MonoBehaviour
 
     private bool _isDragging;
 
+    public bool _isZooming = false;
     private void Awake() {
         _mainCamera = Camera.main;
 
     }
+
+    IEnumerator ZoomCameraToThePath() 
+    {
+        while(true)
+        {
+            if(_isZooming)
+            {
+                float distance = Vector3.Distance(transform.position, GameObject.Find("zoomDestination").transform.position);
+                if(distance > 0.01f)
+                {
+                    transform.position = Vector3.MoveTowards(this.transform.position, GameObject.Find("zoomDestination").transform.position, 0.1f * Time.deltaTime);
+                }
+                else
+                {   
+                    if (GameObject.Find("Background 2"))
+                    {
+                        GameObject.Find("Background 2").SetActive(false);
+                    }
+                   yield return new WaitForSecondsRealtime(1.0f);
+                    _isZooming = false;
+                }
+            }
+            yield return null;
+        }
+    }
+
 
     public void OnDrag(InputAction.CallbackContext ctx) {
         if (ctx.started) _origin = GetMousePosition;
@@ -31,10 +58,16 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        if (!_isDragging) {
+        if (!_isDragging && !_isZooming) {
             playerTransform = GameObject.Find(PLAYER_NAME).transform; 
             transform.position = new Vector3(playerTransform.position.x + shift_x, playerTransform.position.y + shift_y, transform.position.z);
-        } else {
+        } 
+        else if (_isZooming)
+        {
+            Debug.Log("In is zooming");
+            StartCoroutine(ZoomCameraToThePath());
+        }
+        else {
             _difference = GetMousePosition - transform.position;
             transform.position = _origin - _difference;
         }
