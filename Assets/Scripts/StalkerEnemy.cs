f57b3030ad3c00786b502ea4b02bdebc5eedbe92
yuchewang <yc.wang1009@gmail.com>
2023-04-09 01:54:17 -0700

    map: complete level 4

--- Assets/Scripts/StalkerEnemy.cs
+++ Assets/Scripts/StalkerEnemy.cs
@@ -21,9 +21,29 @@ public class StalkerEnemy : MonoBehaviour
     Rigidbody2D myRigidbody;
     // Update is called once per frame
 
+<<<<<<< Updated upstream
     private float originalScale;
 
-    void Start() {
+=======
+    /*  private void OnCollisionEnter2D(Collision2D collision) {
+         if (collision.gameObject.CompareTag("Player"))
+         {
+
+             collision.gameObject.transform.SetParent(transform);
+             //collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+         }
+     }
+>>>>>>> Stashed changes
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    } */
+
+    void Start()
+    {
         myRigidbody = GetComponent<Rigidbody2D>();
         originalScale = transform.localScale.x;
     }
@@ -33,36 +53,37 @@ public class StalkerEnemy : MonoBehaviour
         float distToPlayer = Vector2.Distance(transform.position, player.position);
         if (distToPlayer < argoRange)
         {
-         ChasePlayer(); 
+            ChasePlayer();
         }
         else
         {
-         StopChasingPlayer();
+            StopChasingPlayer();
         }
 
-        
+
 
     }
     void ChasePlayer()
+    {
+        if (transform.position.x < player.position.x)
         {
-            if (transform.position.x < player.position.x)
-            {
-                //enemy is to the left side of the player, so move right
-                myRigidbody.velocity = new Vector2(movespeed, 0);
-                transform.localScale = new Vector2(originalScale,transform.localScale.y);
-            }
-            else
-            {
-                //enemy is to the right side of the player, so move left
-                myRigidbody.velocity = new Vector2(-movespeed, 0);
-                transform.localScale = new Vector2(-originalScale,transform.localScale.y);
-
-            }
+            //enemy is to the left side of the player, so move right
+            myRigidbody.velocity = new Vector2(movespeed, myRigidbody.velocity.y);
+            transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
         }
-
-
-        void StopChasingPlayer()
+        else
         {
-            myRigidbody.velocity = new Vector2(0,myRigidbody.velocity.y + myRigidbody.gravityScale* Time.deltaTime);
+            //enemy is to the right side of the player, so move left
+            myRigidbody.velocity = new Vector2(-movespeed, myRigidbody.velocity.y);
+            transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
+
         }
+    }
+
+
+    void StopChasingPlayer()
+    {
+        if (myRigidbody.velocity.y == 0)
+            myRigidbody.velocity = new Vector2(0, myRigidbody.velocity.y + myRigidbody.gravityScale * Time.deltaTime);
+    }
 }
